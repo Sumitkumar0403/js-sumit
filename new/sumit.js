81a41cb6a4f6be14d7c628596e0366eae4a64452
Sumitkumar0403 <141808260+Sumitkumar0403@users.noreply.github.com>
2023-11-26 09:40:20 +0000

    math

--- new/sumit.js
+++ new/sumit.js
@@ -1,5 +1,9 @@
 "use strict";
 
-let new_=undefined
-let score=Boolean(new_)
-console.log(score);
+const sum=1000000
+// console.log(sum.toFixed(2));
+// console.log(sum.toPrecision(3));
+// console.log(sum.toLocaleString('en-IN'));
+const min=1
+const max=6
+console.log(Math.floor(Math.random()*(max-min))+1)
